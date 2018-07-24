3b581fad0eec4fa53cee803b50add47867dc5da5
Joseph D <josephpdavies@outlook.com>
2018-07-23 20:55:53 -0400

    Changed implementation to be more modular. Now removes comments as well. Activates on mouse movement.

--- related-blocker.js
+++ related-blocker.js
@@ -1,2 +1,13 @@
-var relatedVideos = document.getElementById("related");
-relatedVideos.remove();
+window.onmousemove = function () {
+
+    var removeIDs = ["related", "comments"];
+    var removeListLen = removeIDs.length;
+
+    //Removing all sections of the watch page that have been marked for removal.
+    for (i = 0; i < removeListLen; i++) {
+
+        document.getElementById(removeIDs[i]).remove();
+
+    }
+
+}
